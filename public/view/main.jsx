3805cbd499de331cc018900de6efd73db9603ca8
john li <jetpack@google.com>
2016-03-10 00:38:14 -0500

    Use dict pattern for RegionDetails construction.

--- public/view/main.jsx
+++ public/view/main.jsx
@@ -63,7 +63,12 @@ var selected_regions = new SelectedRegions(function() {
   weather_data_store.on_region_select(selected_regions.get_region_codes());
   // TODO(jetpack): we'll want a similar thing for epi_data_store, I think?
 });
-var region_details = new RegionDetails(rerender, api_client, selected_regions, weather_data_store);
+var region_details = new RegionDetails({
+  on_update: rerender,
+  api_client: api_client,
+  selected_regions: selected_regions,
+  weather_data_store: weather_data_store
+});
 var map_coloring = new MapColoring({
   data_layer: data_layer,
   selected_date: selected_date,
